08518743712dc3daed1305e984e2e93a3ace07a4
Jaguso <jagutzo@gmail.com>
2022-07-08 19:08:04 -0500

    Added some styling

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Main, LeftContainer, RightContainer } from '../styles/pages/Cart.styles';
+import { data } from '../utils/mockData';
 
 export const Cart = () => {
+
+  const [cartProducts, setCartProducts] = useState([]);
+
+  useEffect(() => {
+    const productsSlice = data.data.products.items.slice(0,5);
+    setCartProducts(productsSlice)
+  }, [])
+
+  console.log('cartProducts', cartProducts)
   return (
-    <div>Cart</div>
+    <Main>
+      <LeftContainer></LeftContainer>
+      <RightContainer></RightContainer>
+    </Main>
   )
 }
---
+++ src/styles/pages/Cart.styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+  height: 100vh;
+  background-color: lightblue; 
+  display: flex;
+  padding: 30px
+`
+
+export const LeftContainer = styled.div`
+  width: 80%;
+  height: 100%;
+  background-color: blue;
+`
+
+export const RightContainer = styled.div`
+  width: 20%;
+  height: 100%;
+  background-color: green;
+`
