c926dc28b73827b64f3dd6e476797a33d13af266
Jaguso <jagutzo@gmail.com>
2022-07-15 23:22:55 -0500

    Added redux toolkit

---
+++ src/redux/slices/productsSlice.js
@@ -0,0 +1,13 @@
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+// const URL = 'https://6x8prpit9f.execute-api.us-east-1.amazonaws.com/api/';
+
+// export const getProducts = createAsyncThunk();
+
+
+// export const productsSlice = createSlice({
+//   name: "products",
+//   initialState: {
+//     products: []
+//   }
+// })
