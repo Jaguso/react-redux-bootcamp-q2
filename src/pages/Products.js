3853a6d8b617de07991593dfa035fd6a6cd9846b
Jaguso <jagutzo@gmail.com>
2022-07-16 00:46:47 -0500

    Added products slice

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -3,10 +3,13 @@ import { Main, ProductsContainer } from '../styles/pages/Products.styles';
 import { data } from '../utils/mockData';
 import { ProductCard } from '../components/ProductCard';
 import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { selectAllProducts, getProducts } from '../redux/slices/productsSlice';
 
 
 export const Products = () => {
   let history = useHistory();
+  const dispatch = useDispatch();
   const [products, setProducts] = useState([])
 
   useEffect(() => {
@@ -15,9 +18,13 @@ export const Products = () => {
       history.push('/login')
     } else {
       setProducts(data.data.products.items)
+
+      dispatch(getProducts());
     }
   }, [])
 
+  const productsArr = useSelector(selectAllProducts) 
+  console.log('prod-arr', productsArr)
   return (
     <Main>
       <ProductsContainer>
--- src/redux/slices/productsSlice.js
+++ src/redux/slices/productsSlice.js
@@ -1,13 +1,40 @@
-// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-// const URL = 'https://6x8prpit9f.execute-api.us-east-1.amazonaws.com/api/';
+const URL = 'https://6x8prpit9f.execute-api.us-east-1.amazonaws.com/api/';
 
-// export const getProducts = createAsyncThunk();
+export const getProducts = createAsyncThunk(
+  "products/getProducts",
+  async (_, thunkAPI) => {
+    try {
+      const response = await fetch(`${URL}/products/`, {
+        method: 'GET',
+        headers: {
+          "x-api-key": "lRjxbZfT7i8j77wfef4x75xKvODmnx415yRsYEV5"
+        }
+      });
+      const data = await response.json();
+      return data;
+    } catch(error) {
+
+    }
+  }
+);
 
 
-// export const productsSlice = createSlice({
-//   name: "products",
-//   initialState: {
-//     products: []
-//   }
-// })
+export const productsSlice = createSlice({
+  name: "products",
+  initialState: {
+    products: [],
+    loading: false
+  }, extraReducers: (builder) => {
+    builder
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = action.payload;
+      })
+  }
+});
+
+export const selectAllProducts = (state) => state.products.products;
+
+export default productsSlice.reducer;
